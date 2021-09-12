f7ff495c439ea5fbe802c344c8a8db8084f4df50
tamim-cste <farman0115@student.nstu.edu.bd>
2021-09-12 22:09:56 +0600

    This is third commit

--- first_C.cpp
+++ first_C.cpp
@@ -7,7 +7,8 @@ int main(){
   cin>>a>>b;
   cout<<a+b<<endl;
 
-  cout<<"HI, Welcome to you first time at ubuntu terminal"<<endl;  
+  cout<<"HI, Welcome to you first time at ubuntu terminal"<<endl;
+  cout<<"Congratulations for github login"<<endl;  
   
   return 0;
  }
